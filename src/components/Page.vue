b4deac3d7b396e08b1f9233e8f3a3ae407a5e496
Melle <melle-dev@outlook.com>
2024-06-14 18:10:34 +0200

    fix styling

--- src/components/Page.vue
+++ src/components/Page.vue
@@ -1,5 +1,13 @@
+<script setup lang="ts">
+interface Props {
+  isContentPage?: boolean
+}
+
+defineProps<Props>()
+</script>
+
 <template>
-  <div class="page">
+  <div :class="isContentPage ? 'page-content' : 'page'">
     <slot />
   </div>
 </template>
@@ -9,14 +17,31 @@
   padding-inline: 20px;
 }
 
-.page {
+.page-content {
+  max-width: 1024px;
+  padding-inline: 0;
+  margin-inline: auto;
+  overflow: hidden;
+  background-color: var(--color-background-mute);
+}
+
+.page,
+.page-content {
   margin-top: 70px;
   padding-bottom: 70px;
 }
 
 @media (min-width: 768px) {
-  .page {
+  .page,
+  .page-content {
     margin-top: 100px;
+  }
+}
+
+@media (min-width: 1024px) {
+  .page-content {
+    border-top-left-radius: 25px;
+    border-top-right-radius: 25px;
   }
 }
 </style>
