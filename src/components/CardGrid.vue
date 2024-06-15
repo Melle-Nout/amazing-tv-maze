a8179a94a0dbf0b5fee4b64e4e3d9cbb24b26715
Melle <melle-dev@outlook.com>
2024-06-15 14:59:43 +0200

    Add CardGrid component

---
+++ src/components/CardGrid.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import type { Show } from '@/types/Show'
+import Card from './Card.vue'
+
+interface Props {
+  shows: Show[]
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div class="card-grid">
+    <template v-for="show in shows" :key="`card-${show.id}`">
+      <Card
+        v-if="show.image?.medium || show.image?.original"
+        :id="show.id"
+        :images="{ medium: show.image?.medium, original: show.image?.original }"
+        :rating="show.rating.average"
+      />
+    </template>
+  </div>
+</template>
+
+<style scoped>
+.card-grid {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 20px;
+  margin-top: 20px;
+}
+
+.card {
+  height: 300px;
+  aspect-ratio: 2 / 3;
+}
+</style>
